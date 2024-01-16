6c403b98242ae33d4744877c77d5031fbd9e4468
Kaustubh Agrawal <kaustubhkagrawal@gmail.com>
2024-01-16 17:49:47 +0530

    Basic api call for assistant

---
+++ src/pages/api/assistant.ts
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  message?: string
+  data?: any
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  if (req.method === 'POST') {
+    const { message } = req.body
+
+    const { type = 'function-call', functionCall = {}, call } = message
+    console.log('callObj', call)
+
+    res.status(201).json({ data: functionCall?.parameters })
+
+    res.status(201).json({})
+  }
+
+  res.status(404).json({ message: 'Not Found' })
+}
